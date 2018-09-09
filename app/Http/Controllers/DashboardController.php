bcfa02d4c9e397e6377948f50e37ed1b6d959ffe
Libin Abraham <32189797+libin1993swt@users.noreply.github.com>
2018-09-09 15:42:36 +0530

    Dashboard completed

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,9 +6,31 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use Auth;
+
+use App\URL;
+
 class DashboardController extends Controller
 {
     public function dashboard() {
-    	return view('admin.dashboard');
+    	$name = Auth::user()->name;
+    	$id = Auth::user()->id;
+    	$urls = URL::where('user_id',$id)->get();
+    	$count = sizeof($urls);
+    	$duplicate_count = 0;
+
+    	foreach ($urls as $data) {
+    		$temp = 0;
+    		foreach ($urls as $value) {
+    			$temp = $temp + 1;
+    			if($data->url == $value->url) {
+    				if($temp >=2) {
+    					$duplicate_count = $duplicate_count + 1;
+    				}
+    			}
+    		}
+    	}
+    	return view('admin.dashboard',compact('count','duplicate_count','name'));
     }
 }
+
