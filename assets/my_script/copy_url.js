c8bb9e0acadd0178b07475da1f98cf2c1bb9a48e
Libin Abraham <32189797+libin1993swt@users.noreply.github.com>
2018-09-09 15:51:59 +0530

    Minor change

--- assets/my_script/copy_url.js
+++ assets/my_script/copy_url.js
@@ -1,26 +1,9 @@
-$(document).on('click','.copy',function() {
-	//var short_url = $(this).closest("tr")   // Finds the closest row <tr> 
-  //                     .find(".short_url")
-  //                     .val();     // Gets a descendent with class="nr"  
-	// console.log(short_url);
-	// alert(short_url);
-	// $(".short_url").each(function() {
-  		// var value = $(this).text();
-  		// short_url.select();
-  		// console.log(value);
-  		  // document.execCommand("copy");
-  		  // $(this).select();
-			// $(this).execCommand('copy');
-	// })
-  // copyToClipboard();
-});
 
 function copyToClipboard(element) {
     var msg = $(element).closest("tr").find(".short_url").text();
-    alert(msg);
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val(msg).select();
+    $("#copy_text").val(msg);
+
+    $("#copy_text").select();
     document.execCommand("copy");
-    $temp.remove();
+    alert('short url copied.');
 }
