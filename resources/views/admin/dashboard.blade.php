29b793a8e06bc17ba1d1eb27f6372c8aaecdff15
Libin Abraham <32189797+libin1993swt@users.noreply.github.com>
2018-09-09 15:44:29 +0530

    Dashboard view page updated

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,30 +4,7 @@
 <script src="{{ URL::asset('assets/plugins/jquery-1.10.2.min.js') }}" type="text/javascript"></script>
 <script src="{{ URL::asset('assets/plugins/jquery-gritter/js/jquery.gritter.js') }}" type="text/javascript"></script>
 <script type="text/javascript" src="{{ URL::asset('assets/plugins/data-tables/jquery.dataTables.js') }}"></script>
-<script>
-$(document).ready(function() {
 
-  // $('#demo').DataTable({
-  //
-  //   bProcessing : true,
-  //   bServerSide : true,
-  //   sAjaxSource: "scripts/post.php",
-  //   sServerMethod: "POST",
-  //
-  //
-  //   aLengthMenu: [
-  //     [10, 20, 50, 100, -1],
-  //     [10, 20, 50, 100, "All"]
-  //   ]
-  //
-  // });
-  // var manufacturer_length = $('#manufacturer_length');
-  // var manufacturer_filter = $('#manufacturer_filter');
-
-  // manufacturer_length[0].lastElementChild.firstElementChild.style.float = "right";
-  // manufacturer_length[0].firstElementChild.style.lineHeight = "32px";
-});
-</script>
 <!-- Breadcrumb Section -->
 <div class="row">
   <div class="col-md-12">
@@ -37,11 +14,11 @@ $(document).ready(function() {
     <ul class="page-breadcrumb breadcrumb">
       <li>
         <i class="fa fa-home"></i>
-        <a href="{{url('/admin/dashboard')}}">Home</a>
+        <a href="{{url('/dashboard')}}">Home</a>
         <i class="fa fa-angle-right"></i>
       </li>
       <li>
-        <a href="{{ url('/admin/dashboard')}}">Dashboard</a>
+        <a href="{{ url('/dashboard')}}">Dashboard</a>
       </li>
     </ul>
   </div>
@@ -56,17 +33,17 @@ $(document).ready(function() {
   <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
     <div class="dashboard-stat blue">
       <div class="visual">
-        <i class="fa fa-users"></i>
+        <i class="fa fa-cog"></i>
       </div>
       <div class="details">
         <div class="number">
-           
+           {{ $count }}
         </div>
         <div class="desc">
            Total URLs
         </div>
       </div>
-      <a class="more" href="{{ url('/admin/parts')}}">
+      <a class="more" href="{{ url('/urls')}}">
       View more <i class="m-icon-swapright m-icon-white"></i>
       </a>
     </div>
@@ -74,17 +51,17 @@ $(document).ready(function() {
   <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
     <div class="dashboard-stat green">
       <div class="visual">
-        <i class="fa fa-cogs"></i>
+        <i class="fa fa-cog"></i>
       </div>
       <div class="details">
         <div class="number">
-           
+           {{ $duplicate_count }}
         </div>
         <div class="desc">
            Duplicate URLs
         </div>
       </div>
-      <a class="more" href="{{ url('/admin/manual')}}">
+      <a class="more" href="{{ url('/urls')}}">
       View more <i class="m-icon-swapright m-icon-white"></i>
       </a>
     </div>
